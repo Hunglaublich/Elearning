0ee12cdf4fa691f8ff0fae75888893f577b15207
koetken <konrad.oetken@stud.htwk-leipzig.de>
2022-07-10 00:21:04 +0200

    pokemoncentermap

---
+++ tilesets/pokemoncenter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="pokemoncenter" tilewidth="16" tileheight="16" tilecount="1184" columns="32">
+ <image source="pokemoncenter.png" width="512" height="600"/>
+</tileset>
